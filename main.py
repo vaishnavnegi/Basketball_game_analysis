13e68c4c931990efa9a2b711a61f3b3279cabe9b
vaishnavnegi <vaishnavnegi207@gmail.com>
2025-05-19 13:10:37 +0200

    Tested out the util file from main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from ultralytics import YOLO
-# model = YOLO("yolov8x.pt")  # Load a pretrained model (or specify a custom path)
-# model = YOLO("models/Player_Detector.pt")  # Load a pretrained model (or specify a custom path)
-model = YOLO("models/Ball_Detector.pt")  # Load a pretrained model (or specify a custom path)
-# results = model.predict("input_videos/video_1.mp4", save=True)  # Predict on webcam (or specify a custom video path)
-results = model.track("input_videos/video_1.mp4", save=True)  
-print(results)  # Print results to console
-print("=======================")
-for box in results[0].boxes:
-    print(f"Box coordinates: {box.xyxy}")  # Print box coordinates
-    print(f"Box confidence: {box.conf}")  # Print box confidence
-    print(f"Box class: {box.cls}")  # Print box class
-    print("=======================")
+from utils import read_video, save_video
+
+def main():
+
+    # Read Video
+    video_frames = read_video("input_videos/video_1.mp4")
+
+    # Save video
+    save_video(video_frames, "output_videos/video_1.mp4")
+
+if __name__ == '__main__':
+    main()
