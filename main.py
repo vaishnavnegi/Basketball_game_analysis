18b469ff324f7980a54a3b1ea2516fc943e51559
vaishnavnegi <vaishnavnegi207@gmail.com>
2025-06-21 23:45:01 +0200

    Added team assignment code using a 0 shot classifier from Hugging face. Added logic to drawers to refelct team assignment using different colors. Tested in main. Output video OK.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
 from drawers import PlayerTracksDrawer, BallTracksDrawer
+from team_assignment import TeamAssigner
 
 def main():
 
@@ -23,18 +24,26 @@ def main():
                                                  read_from_stub=True,
                                                  stub_path="stubs/ball_tracks.pkl"
                                                  )
-
+    # Remove wrong detections
     ball_tracks = ball_tracker.remove_wrong_detections(ball_tracks)
-    
+    # Interpolate Ball Positions
     ball_tracks = ball_tracker.interpolate_ball_positions(ball_tracks)
 
+    # Assign Teams to Players
+    team_assigner = TeamAssigner()
+    player_assignment = team_assigner.get_player_teams_across_frames(video_frames, 
+                                                                player_tracks,                                                        
+                                                                read_from_stub=True,
+                                                                stub_path="stubs/player_teams.pkl"
+                                                                )
+    
     #Drawing Player Tracks
     #Initialize drawers
     player_tracks_drawer = PlayerTracksDrawer()
     ball_tracks_drawer = BallTracksDrawer()
     
     # Drawing Player Tracks
-    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
+    output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks, player_assignment)
     
     # Drawing Ball Tracks
     output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
---
+++ team_assignment/__init__.py
@@ -0,0 +1,1 @@
+from .assign_teams import TeamAssigner
